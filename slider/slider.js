44abcd048818ec899f5a11925eeafb075b69b2fe
Vladislav <vladislove1217@gmail.com>
2022-01-16 05:25:00 +0600

    final

--- slider/slider.js
+++ slider/slider.js
@@ -1,118 +1,38 @@
-var slider = document.getElementById('slider'),
-    sliderItems = document.getElementById('slides'),
-    prev = document.getElementById('prev'),
-    next = document.getElementById('next');
+const carousel = document.querySelector('.carousel');
+const slider = document.querySelector('.slider');
+const width = slider
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+let direction;
 
-function slide(wrapper, items, prev, next) {
-  var posX1 = 0,
-      posX2 = 0,
-      posInitial,
-      posFinal,
-      threshold = 100,
-      slides = items.getElementsByClassName('slide'),
-      slidesLength = slides.length,
-      slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
-      firstSlide = slides[0],
-      lastSlide = slides[slidesLength - 1],
-      cloneFirst = firstSlide.cloneNode(true),
-      cloneLast = lastSlide.cloneNode(true),
-      index = 0,
-      allowShift = true;
-  
-  // Clone first and last slide
-  items.appendChild(cloneFirst);
-  items.insertBefore(cloneLast, firstSlide);
-  wrapper.classList.add('loaded');
-  
-  // Mouse events
-  items.onmousedown = dragStart;
-  
-  // Touch events
-  items.addEventListener('touchstart', dragStart);
-  items.addEventListener('touchend', dragEnd);
-  items.addEventListener('touchmove', dragAction);
-  
-  // Click events
-  prev.addEventListener('click', function () { shiftSlide(-1) });
-  next.addEventListener('click', function () { shiftSlide(1) });
-  
-  // Transition events
-  items.addEventListener('transitionend', checkIndex);
-  
-  function dragStart (e) {
-    e = e || window.event;
-    e.preventDefault();
-    posInitial = items.offsetLeft;
-    
-    if (e.type == 'touchstart') {
-      posX1 = e.touches[0].clientX;
-    } else {
-      posX1 = e.clientX;
-      document.onmouseup = dragEnd;
-      document.onmousemove = dragAction;
-    }
+next.addEventListener('click', function() {
+  direction = -1;
+  carousel.style.justifyContent = 'flex-start';
+  slider.style.transform = 'translate(-20%)';  
+});
+
+prev.addEventListener('click', function() {
+  if (direction === -1) {
+    direction = 1;
+    slider.appendChild(slider.firstElementChild);
   }
+  carousel.style.justifyContent = 'flex-end';    
+  slider.style.transform = 'translate(20%)';  
+  
+});
 
-  function dragAction (e) {
-    e = e || window.event;
-    
-    if (e.type == 'touchmove') {
-      posX2 = posX1 - e.touches[0].clientX;
-      posX1 = e.touches[0].clientX;
-    } else {
-      posX2 = posX1 - e.clientX;
-      posX1 = e.clientX;
-    }
-    items.style.left = (items.offsetLeft - posX2) + "px";
+slider.addEventListener('transitionend', function() {
+  // get the last element and append it to the front
+  
+  if (direction === 1) {
+    slider.prepend(slider.lastElementChild);
+  } else {
+    slider.appendChild(slider.firstElementChild);
   }
   
-  function dragEnd (e) {
-    posFinal = items.offsetLeft;
-    if (posFinal - posInitial < -threshold) {
-      shiftSlide(1, 'drag');
-    } else if (posFinal - posInitial > threshold) {
-      shiftSlide(-1, 'drag');
-    } else {
-      items.style.left = (posInitial) + "px";
-    }
-
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-  
-  function shiftSlide(dir, action) {
-    items.classList.add('shifting');
-    
-    if (allowShift) {
-      if (!action) { posInitial = items.offsetLeft; }
-
-      if (dir == 1) {
-        items.style.left = (posInitial - slideSize) + "px";
-        index++;      
-      } else if (dir == -1) {
-        items.style.left = (posInitial + slideSize) + "px";
-        index--;      
-      }
-    };
-    
-    allowShift = false;
-  }
-    
-  function checkIndex (){
-    items.classList.remove('shifting');
-
-    if (index == -1) {
-      items.style.left = -(slidesLength * slideSize) + "px";
-      index = slidesLength - 1;
-    }
-
-    if (index == slidesLength) {
-      items.style.left = -(1 * slideSize) + "px";
-      index = 0;
-    }
-    
-    allowShift = true;
-  }
-}
-
-slide(slider, sliderItems, prev, next);
+  slider.style.transition = 'none';
+  slider.style.transform = 'translate(0)';
+  setTimeout(() => {
+    slider.style.transition = 'all 0.5s';
+  })
+}, false);
