8e80db80e2c34187e55c256260ba55c33ff15b14
Vladislav <vladislove1217@gmail.com>
2022-01-20 00:21:48 +0600

    added infinity scroll

--- slider/slider.js
+++ slider/slider.js
@@ -1,38 +1,81 @@
-const carousel = document.querySelector('.carousel');
-const slider = document.querySelector('.slider');
-const width = slider
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev');
-let direction;
+const slides = document.querySelectorAll('.slide'),
+    prev = document.querySelector('#leftArrow'),
+    next = document.querySelector('#rightArrow'),
+    sliderWrapper = document.querySelector('.slider-wrapper'),
+    sliderInner = document.querySelector('.slider-inner'),
+    width = +window.getComputedStyle(sliderWrapper).width.match(/\d+/g)[0];
 
-next.addEventListener('click', function() {
-  direction = -1;
-  carousel.style.justifyContent = 'flex-start';
-  slider.style.transform = 'translate(-20%)';  
+const firstSlide = slides[0],
+    slidesLength = slides.length,
+    lastSlide = slides[slidesLength - 1],
+    firstClone = firstSlide.cloneNode(true),
+    lastClone = lastSlide.cloneNode(true);
+
+sliderInner.prepend(lastClone);
+sliderInner.append(firstClone);
+
+let offset = 0;
+let allowAnim = true,
+animTime = 0;
+
+sliderInner.style.width = 100 * (slides.length + 2) + '%';
+sliderInner.style.left = `-${width}px`;
+sliderInner.style.display = 'flex';
+sliderWrapper.style.overflow = 'hidden';
+let firstPos = 0;
+let lastPos = 0;
+
+slides.forEach(slide => {
+    slide.style.width = width;
+})
+
+
+next.addEventListener('click', () => {
+    firstPos = lastPos;
+    lastPos+=width;
+    if (sliderInner.style.transform == `translateX(-${width * (slidesLength-1)}px)`) {
+        const nextAnim = setInterval(() => {
+            firstPos+=10;
+            sliderInner.style.transform = `translateX(-${firstPos}px)`;
+            if (firstPos >= lastPos) {
+                clearInterval(nextAnim);
+                sliderInner.style.transform = `translateX(0px)`;
+                lastPos = 0;
+            }
+        }, 10)
+    } else {
+        const nextAnim = setInterval(() => {
+            firstPos+=10;
+            sliderInner.style.transform = `translateX(-${firstPos}px)`;
+            if (firstPos >= lastPos) {
+                clearInterval(nextAnim);
+            }
+        }, 10)
+    }
 });
 
-prev.addEventListener('click', function() {
-  if (direction === -1) {
-    direction = 1;
-    slider.appendChild(slider.firstElementChild);
-  }
-  carousel.style.justifyContent = 'flex-end';    
-  slider.style.transform = 'translate(20%)';  
-  
-});
 
-slider.addEventListener('transitionend', function() {
-  // get the last element and append it to the front
-  
-  if (direction === 1) {
-    slider.prepend(slider.lastElementChild);
-  } else {
-    slider.appendChild(slider.firstElementChild);
-  }
-  
-  slider.style.transition = 'none';
-  slider.style.transform = 'translate(0)';
-  setTimeout(() => {
-    slider.style.transition = 'all 0.5s';
-  })
-}, false);
+prev.addEventListener('click', () => {
+    firstPos = lastPos;
+    lastPos-=width;
+    if (sliderInner.style.transform == 'translateX(0px)' ||
+    window.getComputedStyle(sliderInner).transform == 'none') {
+        const prevAnim = setInterval(() => {
+            firstPos-=10;
+            sliderInner.style.transform = `translateX(${-firstPos}px)`;
+            if (firstPos <= lastPos) {
+                clearInterval(prevAnim);
+                sliderInner.style.transform = `translateX(-${width * (slidesLength - 1)}px)`;
+                lastPos = width * (slidesLength - 1);
+            }
+        }, 10) 
+        } else {
+            const prevAnim = setInterval(() => {
+                firstPos-=10;
+                sliderInner.style.transform = `translateX(${-firstPos}px)`;
+                if (firstPos <= lastPos) {
+                    clearInterval(prevAnim);
+                }
+            }, 10)
+    }
+});
