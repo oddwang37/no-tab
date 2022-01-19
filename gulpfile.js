c8dd8e1408d096a28c23021e48d2f9c43a187cd9
Vladislav <vladislove1217@gmail.com>
2022-01-20 05:53:27 +0600

    final

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('html', function () {
         .pipe(gulp.dest("dist/"));
 });
 
-gulp.task('scripts', function () {
+gulp.task('scripts', function () { 
     return gulp.src("src/js/**/*.js")
         .pipe(gulp.dest("dist/js"));
 });
